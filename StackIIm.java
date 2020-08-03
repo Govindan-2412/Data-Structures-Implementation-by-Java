b34564b5375b151db772303835326f2eaaeeaff3
Govindan Palani <67826158+Govindan-2412@users.noreply.github.com>
2020-08-03 17:17:30 +0530

    Stack Implementation by using LinkedList

---
+++ StackIIm.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Govindan
+ */
+public class StackIIm {
+    public static void main(String args[])throws Exception{
+        Stacklinke in=new Stacklinke();
+            in.push(12);
+            in.push(12);
+            in.push(23);
+            System.out.println(in.peek());
+        }
+    }
+    
+}
